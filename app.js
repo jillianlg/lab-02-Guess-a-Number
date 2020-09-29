97b88c40c278ac045346467634a474dca6242c41
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-09-29 14:13:56 -0700

    add html elements, add getElementById var and validate

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 // import functions and grab DOM elements
+const guessesRemain = document.getElementById('guesses-span');
+const guessHighLow = document.getElementById('high-low-span');
+const guessButton = document.getElementById('guess-button');
+const results = document.getElementById('results-span');
 
+console.log(guessesRemain);
+console.log(guessHighLow);
+console.log(guessButton);
+console.log(results);
 // initialize state
 
 // set event listeners to update state and DOM

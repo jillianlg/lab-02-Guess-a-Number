7c9e546d3b5dbd4ab60f2b63ca856f6f24cc9086
Jillian Gibson <jillian.l.gibson@gmail.com>
2020-09-29 14:26:18 -0700

    add input to index.html, add button and EventListener, validate button decrease incrementally

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const guessHighLow = document.getElementById('high-low-span');
 const guessButton = document.getElementById('guess-button');
 const results = document.getElementById('results-span');
 
-console.log(guessesRemain);
-console.log(guessHighLow);
-console.log(guessButton);
-console.log(results);
 // initialize state
-
+let guesses = 4;
 // set event listeners to update state and DOM
+
+guessButton.addEventListener('click', () => {
+    guesses--;
+  
+  console.log(guesses);
+});
